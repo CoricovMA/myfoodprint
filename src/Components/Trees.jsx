4b8d87bd865441f394ed7debf1215445f1cb81d3
CoricovMA <coricovacMA@gmail.com>
2020-08-20 13:11:21 -0400

    started working on trees

---
+++ src/Components/Trees.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import autumn from '../Assets/Trees/autumn_tree.svg'
+import winter from '../Assets/Trees/winter_tree.svg'
+import summer from '../Assets/Trees/summer_tree.svg'
+import spring from '../Assets/Trees/spring_tree.svg'
+
+export default function Trees(){
+
+
+}
