be30705b4508c042766583c22a92defa03db5f55
CoricovMA <coricovacMA@gmail.com>
2020-08-20 19:35:03 -0400

    still working on tree timer

--- src/Components/Trees.jsx
+++ src/Components/Trees.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import autumn from '../Assets/Trees/autumn_tree.svg'
 import winter from '../Assets/Trees/winter_tree.svg'
 import summer from '../Assets/Trees/summer_tree.svg'
@@ -6,7 +6,6 @@ import spring from '../Assets/Trees/spring_tree.svg'
 import {Image, Row, Col, Container, Jumbotron} from 'react-bootstrap'
 
 function TreeComponent(props) {
-
 
     return (
         <Col>
@@ -20,10 +19,12 @@ function TreeComponent(props) {
 
 export default function Trees() {
     const treeSVG = [spring, summer, autumn, winter];
+    const [trees, setTrees] = useState([])
+    const [count, setCount] = useState(0)
 
     return (
                 <Row>
-                    {treeSVG.map(
+                    {trees.map(
                         tree => <TreeComponent image={tree}/>
                     )}
                 </Row>
