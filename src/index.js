a1c6fb2736b92baa5fc3cb020c88969fa068fe43
CoricovMA <coricovacMA@gmail.com>
2020-08-24 23:28:29 -0400

    added https redirect

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <HttpsRedirect>
+          <App />
+      </HttpsRedirect>
   </React.StrictMode>,
   document.getElementById('root')
 );
